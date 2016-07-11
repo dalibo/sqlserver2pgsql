a8338fb605ed15f240da13330333300c23e43809
Javier Callico <jcallico@callicode.com>
2016-07-11 13:13:07 -0400

    Making regex less greedy.

--- sqlserver2pgsql.pl
+++ sqlserver2pgsql.pl
@@ -404,7 +404,7 @@ sub convert_transactsql_code
 	$code =~ s/[\[\]]/\"/gi;
 	$code =~ s/getdate\s*\(\)/CURRENT_TIMESTAMP/gi;
 	$code =~ s/user_name\s*\(\)/CURRENT_USER/gi;
-	$code =~ s/datepart\s*\(\s*(.*)\s*\,\s*(.*)\s*\)/date_part('$1', $2)/gi;
+	$code =~ s/datepart\s*\(\s*(.*?)\s*\,\s*(.*?)\s*\)/date_part('$1', $2)/gi;
 	return $code;
 }
 
